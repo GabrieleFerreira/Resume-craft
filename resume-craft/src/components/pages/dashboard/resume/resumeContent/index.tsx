b1d460d52d5fe53aadd3b3e5feaf97d5125a7e0f
GabrieleFerreira <gabriele.ferreira@ufvjm.edu.br>
2025-11-05 18:02:57 -0300

    layout cabeçalho do curriculo

--- resume-craft/src/components/pages/dashboard/resume/resumeContent/index.tsx
+++ resume-craft/src/components/pages/dashboard/resume/resumeContent/index.tsx
@@ -1,5 +1,26 @@
+"use client";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { TransformControls } from "./Controls";
+import { NavigationHeader } from "./NavigationHeader";
+import { Resumetemplate } from "./templates";
 export const ResumeContent = () => {
   return (
-    <section className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background "></section>
+    <section className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background ">
+      <TransformWrapper
+        initialScale={0.5}
+        minScale={0.4}
+        centerOnInit
+        centerZoomedOut
+        limitToBounds={false}
+      >
+        <>
+          <TransformControls />
+          <NavigationHeader />
+          <TransformComponent>
+            <Resumetemplate />
+          </TransformComponent>
+        </>
+      </TransformWrapper>
+    </section>
   );
 };
---
+++ resume-craft/src/components/pages/dashboard/resume/resumeContent/templates/index.tsx
@@ -0,0 +1,8 @@
+export const Resumetemplate = () => {
+  return (
+    <div
+      className="w-[210mm] min-h-[297mm] bg-white text-black font-arial [&_hr]:border-black"
+      id="resume-content"
+    ></div>
+  );
+};
